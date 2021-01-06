279e5301a0d9a75f2a6654ca51a0064c32823267
Lars Kjeldgaard <lars_kjeldgaard@hotmail.com>
2021-01-06 19:02:09 +0100

    better functionality for deploying models

--- deploy_models.py
+++ deploy_models.py
@@ -4,47 +4,30 @@ import pickle
 import torch
 import boto3
 
-def train_save(transformer = 'bert-base-multilingual-uncased',
-               alias = 'mbert',
-               hyperparameters = {'epochs' : 4,
-                                  'warmup_steps' : 500,
-                                  'train_batch_size': 13,
-                                  'learning_rate': 0.0001},
-               limit = None,
-               to_s3 = False):
-    
-    # instantiate model.
-    model = NERDA(transformer = transformer,
-                  dataset_training = get_dane_data('train', limit = limit), 
-                  dataset_validation = get_dane_data('dev', limit = limit),
-                  hyperparameters = {'epochs' : 4,
-                                     'warmup_steps' : 500,
-                                     'train_batch_size': 13,
-                                     'learning_rate': 0.0001})
-    model.train()
+def deploy_model_to_s3(model, test_set = get_dane_data('test')):
 
-    # save model.
-    file_model = f'{alias}_model.bin'
+    model_name = type(model).__name__
+
+    file_model = f'{model_name}.bin'
     torch.save(model.network.state_dict(), file_model)
     
     # compute performance on test set and save.
-    dataset_test = get_dane_data('test', limit = limit)
-    f1 = model.evaluate_performance(dataset_test)
-    file_f1 = f'{alias}_f1.pickle'
-    with open(file_f1, 'wb') as file:
-        pickle.dump(f1, file)
+    performance = model.evaluate_performance(test_set)
+    # TODO: save as .csv
+    file_performance = f'{model_name}_performance.pickle'
+    with open(file_performance, 'wb') as file:
+        pickle.dump(file_performance, file)
     
-    # save in S3 bucket.
-    if to_s3:
-        s3 = boto3.resource('s3')
-        s3.Bucket('larsktest').upload_file(
+    # upload to S3 bucket.
+    s3 = boto3.resource('s3')
+    s3.Bucket('nerda').upload_file(
             Filename=file_model, 
-            Key = f'NERDA/{file_model}')
-        s3.Bucket('larsktest').upload_file(
-            Filename=file_f1, 
-            Key = f'NERDA/{file_f1}')
+            Key = file_model)
+    s3.Bucket('nerda').upload_file(
+            Filename=file_performance, 
+            Key = file_performance)
 
-    return model, f1   
+    return "Model deployed to S3 successfully."   
 
 cfg_mbert = {
     'transformer': 'bert-base-multilingual-uncased',
