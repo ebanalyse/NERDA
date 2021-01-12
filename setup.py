5308e5a0d72a2462ad74162bf1da716393af4777
Lars Kjeldgaard <lars_kjeldgaard@hotmail.com>
2021-01-12 16:21:53 +0100

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="NERDA", 
-    version="0.0.33",
+    version="0.0.40",
     author="PIN",
     author_email="lars.kjeldgaard@eb.dk",
     description="A Framework for Finetuning Transformers for Named Entity Recognition",
